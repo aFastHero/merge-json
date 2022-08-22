641e7d89d6ce04fda29b6fe457d70f2a9e708c03
aFastHero <aFastHero@protonmail.com>
2022-08-21 23:06:14 -0500

    main

--- main.py
+++ main.py
@@ -3,37 +3,50 @@ import os.path
 
 
 def main():
+
     fileList = []
-    path = 'sample_movie'
+    path = 'Metadata'
     for filenames in os.walk(path):
+        
         fileList.append(filenames)
 
     data_list = []
-
     for i in range(len(filenames[2])):
+        
         try:
-            data = json.load(open('sample_movie/' + filenames[2][i], encoding='utf-8'))
-            data_final = dict((k, data[k]) for k in ('original_title', 'budget', 'genres', 'popularity', "release_date",
-                                                     "revenue", "runtime", "vote_average", "vote_count", "spoken_languages"))
-            genres = []
-            for i in range(len(data_final["genres"])):
-                genres.append(data_final["genres"][i]['name'])
-            data_final["genres"] = ", ".join(genres)
 
-            spoken_languages = []
-            for i in range(len(data_final["spoken_languages"])):
-                genres.append(data_final["spoken_languages"][i]['name'])
-            data_final["spoken_languages"] = ", ".join(spoken_languages)
+            data = json.load(open('Metadata/' + filenames[2][i], encoding='utf-8'))
+            data_final = dict((k, data[k]) for k in ("attributes","image","name","description"))
+
+            attributes = []
+            for i in range(len(data_final["attributes"])):
+
+                try:
+                    
+                    props = attributes[i]
+                    props_final = dict((k,props[k]) for k in ("trait_type","value"))
+                    
+                    trait_list = []
+                    for i in range(len(attributes)):
+                        
+                        trait_list.append(props_final["trait_type"][i]["value"])
+                    props_final["trait_type"] = trait_list
+
+                    attributes.append(props_final)
+                except Exception:
+                    pass
+
+                attributes.append(data_final["attributes"][i])
+            data_final["attributes"] = attributes
 
             data_list.append(data_final)
         except Exception:
             pass
 
-    out_file = open("movies.json", "w")
+    out_file = open("metadata.json", "w")
     json.dump(data_list, out_file, indent=2)
     out_file.close()
 
 
 if __name__ == '__main__':
     main()
-
